aa9a26e80a5fc2553a689063353125029a8d25f6
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-05-10 20:10:59 +0530

    Update Appcode.py

--- Appcode.py
+++ Appcode.py
@@ -14,6 +14,7 @@ def get_weather(city):
     temp = data["main"]["temp"]
     humidity = data["main"]["humidity"]
 
+
     # format and print the weather information
     print("Weather in {}:".format(city))
     print(" - Condition: {}".format(weather))
