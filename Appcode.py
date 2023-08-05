fdee4de8de8b88562f10bdc667b09395c5b299be
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-08-06 00:02:51 +0530

    Update Appcode.py

--- Appcode.py
+++ Appcode.py
@@ -2,12 +2,14 @@ import requests
 import json
 
 
+
 api_key = "API_KEY"  # replace with your own API key from OpenWeatherMap
 
 def get_weather(city):
     # make a request to the OpenWeatherMap API
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city, api_key)
     response = requests.get(url)
+
 
     # parse the JSON response
     data = json.loads(response.text)
@@ -17,11 +19,14 @@ def get_weather(city):
 
 
 
+
     # format and print the weather information
     print("Weather in {}:".format(city))
     print(" - Condition: {}".format(weather))
     print(" - Temperature: {}°F".format(round(temp * 9/5 - 459.67, 2)))
     print(" - Humidity: {}%".format(humidity))
+
+
 
 # test the function with a city name
 get_weather("Delhi")
